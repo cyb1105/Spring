650ce86fbe5b97d24c9f68137caab26ad5d30c2e
yoonbyeong chae <dbsqud1105@gmail.com>
2020-05-04 17:50:45 +0900

    springboot

---
+++ Spring boot/react_boot/src/main/resources/static/precache-manifest.1f8f5c19061cf78da2ac2c8c65c6d97c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87eea6fc478af8519ab573229e2e6a79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a00a1620b22c161709a",
+    "url": "/static/css/main.3b68c6a2.chunk.css"
+  },
+  {
+    "revision": "fd3b4cf12561ce318d5b",
+    "url": "/static/js/2.3fe816d9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3fe816d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a00a1620b22c161709a",
+    "url": "/static/js/main.471fea2e.chunk.js"
+  },
+  {
+    "revision": "e02c5f3992f76a983bca",
+    "url": "/static/js/runtime-main.ca391f6e.js"
+  }
+]);
